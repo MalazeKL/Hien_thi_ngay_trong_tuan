4ae13557080ff5a3e5b9f0616490b9b7e2aab3a6
Your Name <Malaze0933@gmail.com>
2022-10-24 18:55:43 +0700

    hien thi ngay trong tuan

---
+++ hien_thi_ngay_trong_tuan.py
@@ -0,0 +1,18 @@
+num = int(input("Nhập số (từ 1 đến 7): "))
+
+if num == 1:
+    print("thứ 2")
+elif num == 2:
+    print("thứ 3")
+elif num == 3:
+    print("thứ 4")
+elif num == 4:
+    print("thứ 5")
+elif num == 5:
+    print("thứ 6")
+elif num == 6:
+    print("thứ 7")
+elif num == 7:
+    print("chủ nhật")
+else:
+    print("số", num, "không hợp lệ! (chỉ được nhập số từ 1 đến 7)")
